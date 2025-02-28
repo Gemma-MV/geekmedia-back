946bfbf662a24d599b5f018bf1270a22a289bc4c
gemmavalenciano <gemmavalenciano83@gmail.com>
2025-02-28 20:20:57 +0100

    Finalizacion de peticion de imagenes a la base de datos

--- routes/routes.js
+++ routes/routes.js
@@ -7,9 +7,12 @@ import music from '../controllers/musicController.js';
 
 const router = Router();
 
+const app = express();
+
 //Enpoint de conexion
 router.get('/geekmedia', genericControler);
 router.get('/geekmedia/books/all-books', books.allBooks);
+// router.get('/geekmedia/books/one-book', books.oneBook);
 router.get('/geekmedia/movies/all-movies', movies.allMovies);
 router.get('/geekmedia/music/all-music', music.allMusic);
 
